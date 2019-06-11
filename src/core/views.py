47cc620a82508d165250de3dfe8510a5fca44e39
williamabreu <williamsantos_00@hotmail.com>
2019-06-10 22:36:39 -0300

    Adicionado controller cadastro de plano

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('OK!')
+    if request.method == 'GET':
+        return render(request, 'site/index.html')
+    if request.method == 'POST':
+        plan = models.Plan()
+        plan.plan_name = request.POST['name']
+        plan.price = request.POST['price']
+        plan.save()
+        return HttpResponse('ok!')
