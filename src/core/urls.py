e77c0ca5ba4270a235a883b38a953c54146e7ce0
williamabreu <williamsantos_00@hotmail.com>
2019-06-11 17:12:46 -0300

    Implementada modificador de plano
    Modifica uma entrada já existente na tabela de plano. Versão parcial.
    
    Referência #51 #61

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('cadastrar-plano/', views.create_plan, name='create_plan'),
-    # path('modificar-plano/', views.create_plan, name='update_plan'),
+    path('modificar-plano/<int:id>/', views.update_plan, name='update_plan'),
     # path('listar-planos/', views.index, name='listar-planos'),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -15,3 +15,15 @@ def create_plan(request):
         plan.price = request.POST['price']
         plan.save()
         return redirect('create_plan')
+
+def update_plan(request, id):
+    plan = models.Plan.objects.get(id=id)
+    
+    if request.method == 'GET':
+        return render(request, 'site/update_plan.html', context={'plan': plan})
+    
+    if request.method == 'POST':
+        plan.plan_name = request.POST['name']
+        plan.price = request.POST['price']
+        plan.save()
+        return redirect('update_plan', id=id)
