f7a2053ad87a8cbbacfb2e6f0e7ad8c868c30a89
williamabreu <williamsantos_00@hotmail.com>
2019-06-30 00:32:26 -0300

    Adicionada configuração para arquivos estáticos

--- src/sysadmin/urls.py
+++ src/sysadmin/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('AdminFinanceiro.urls')),
     path('', include('Atendente.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
