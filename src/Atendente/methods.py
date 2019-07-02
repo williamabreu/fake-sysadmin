5ca97766074306ac0d495ae47bf754d41b5a28c5
williamabreu <williamsantos_00@hotmail.com>
2019-07-01 22:09:51 -0300

    Adicionada camada methods para facilitar teste
    Abstrai procedimentos de manipulação de dados para ficar mais
    fácil de usar tanto nas views quanto nos tests

---
+++ src/Atendente/methods.py
@@ -0,0 +1,60 @@
+"""
+Abstração de métodos usados nas views (controllers) do CRUD do Atendente.
+"""
+from . import models
+from datetime import datetime
+
+
+def create_client(name, cpf, rg, birthday, address, install_date, install_hour, plan_id):
+    person = models.Person()
+    person.name = name
+    person.cpf = cpf
+    person.rg = rg
+    person.birthday = birthday
+    person.address = address
+    person.save()
+
+    installation = models.Schedule()
+    installation.date = install_date
+    installation.hour = install_hour
+    installation.save()
+
+    contract = models.Contract()
+    contract.subscription_date = datetime.now()
+    contract.plan = models.Plan.objects.get(id=plan_id)
+    contract.save()
+
+    client = models.Client()
+    client.person = person
+    client.install_date = installation
+    client.contract = contract
+    client.save()
+
+
+def list_all_clients():
+    return models.Client.objects.all()
+
+
+def update_client(client, posted_plan_id, name, cpf, rg, birthday, address):
+    if posted_plan_id != client.contract.plan.id:
+        # Altera o plano de assinatura
+        contract = models.Contract()
+        contract.subscription_date = datetime.now()
+        contract.plan = models.Plan.objects.get(id=posted_plan_id)
+        contract.save()
+
+        client.contract.delete()
+        client.contract = contract
+        client.save()
+
+    client.person.name = name
+    client.person.cpf = cpf
+    client.person.rg = rg
+    client.person.birthday = birthday
+    client.person.address = address
+    client.person.save()
+
+
+def delete_client_subscription(client):
+    client.contract.delete()
+    client.person.delete()
